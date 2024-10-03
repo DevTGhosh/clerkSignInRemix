270be378f748e11fcfc2157e99618955e7e705ea
Devjyoti <devjyotighosh02@gmail.com>
2024-10-03 12:57:40 +0530

    Start fresh

--- app/routes/dashboard.tsx
+++
@@ -1,8 +0,0 @@
-export default function Dashboard() {
-  return (
-    <div className='flex flex-col w-full min-h-screen py-4 bg-muted/40'>
-      <h1>Dashboard</h1>
-      <p>You are signed in!</p>
-    </div>
-  )
-}
